20c2a26471fde87b0a83f1469518132ce7de51e7
HucPhoang <phuchoangtran2806@gmail.com>
2023-06-07 01:40:46 +0700

    Allowing customize duration and enable long break mode

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,55 @@
 const DEFAULT_FOCUS_DURATION = 25;
-const DEFAULT_BREAK_DURATION = 5;
+const DEFAULT_SHORT_BREAK_DURATION = 5;
+const DEFAULT_LONG_BREAK_DURATION = 15;
+const DEFAULT_NUMBERS_CYCLE = 4;
 
 var focusDuration;
-var breakDuration;
+var shortBreakDuration;
+var longBreakDuration;
+var numbersCycle;
 var timeRemaining;
 var isFocus;
+var isEnableLongBreak;
 
-function pageLoad() {
-    document.getElementById('focus-duration').value = DEFAULT_FOCUS_DURATION.toString();
-    document.getElementById('short-break-duration').value = DEFAULT_BREAK_DURATION.toString();
-    focusDuration = parseInt(document.getElementById('focus-duration').value) * 60;
-    breakDuration = parseInt(document.getElementById('short-break-duration').value) * 60;
+const inputFocusDuration = document.getElementById('focus-duration');
+const inputShortBreakDuration = document.getElementById('short-break-duration');
+const inputLongBreakDuration = document.getElementById('long-break-duration');
+const inputNumbersCycle = document.getElementById('cycles');
+const displayTimerNumber = document.getElementById('display__duration');
+const checkboxLongBreak = document.getElementById('enable-long-break');
+
+function pageLoading(isSetToDefault = true) {
+    if (isSetToDefault) {
+        inputFocusDuration.value = DEFAULT_FOCUS_DURATION.toString();
+        inputShortBreakDuration.value = DEFAULT_SHORT_BREAK_DURATION.toString();
+        inputLongBreakDuration.value = DEFAULT_LONG_BREAK_DURATION.toString();
+        inputNumbersCycle.value = DEFAULT_NUMBERS_CYCLE.toString();
+        checkboxLongBreak.checked = false;
+        document.querySelector('#long-break-box').style.opacity = 0.5;
+        inputLongBreakDuration.disabled = false;
+        inputNumbersCycle.disabled = false;
+    }
+
+    document.getElementById('countdown-bar').style.background = `conic-gradient(var(--body-background-color) 0deg, var(--countdown-cycle-bar-color) 0deg)`;
+
+    focusDuration = parseInt(inputFocusDuration.value) * 60;
+    shortBreakDuration = parseInt(inputShortBreakDuration.value) * 60;
+    longBreakDuration = parseInt(inputLongBreakDuration.value) * 60;
+    numbersCycle = parseInt(inputNumbersCycle.value);
+
     timeRemaining = focusDuration;
     isFocus = true;
     isPause = false;
+    isEnableLongBreak = checkboxLongBreak.checked;
+
     let minutes = Math.floor(timeRemaining / 60);
     let seconds = timeRemaining - minutes * 60;
 
-    const timerNumber = document.getElementById('display__duration');
-    timerNumber.innerHTML = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    displayTimerNumber.innerHTML = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 }
 
 const startButton = document.getElementById('play-btn');
 startButton.addEventListener('click', () => {
-    const timerNumber = document.getElementById('display__duration');
     const timerMode = document.getElementById('display__mode');
     const countdownBar = document.getElementById('countdown-bar');
     const pauseButton = document.getElementById('pause-btn');
@@ -34,34 +60,54 @@ startButton.addEventListener('click', () => {
     startButton.style.visibility = 'hidden';
     pauseButton.style.visibility = 'visible';
 
-    const timervisibility = () => {
+    var countdownBarRatios = [focusDuration, shortBreakDuration, longBreakDuration];
+
+    const timerVisibility = () => {
         let minutes = Math.floor(timeRemaining / 60);
         let seconds = timeRemaining - minutes * 60;
-        let countdownBarRatio = 360 / (isFocus ? focusDuration : breakDuration);
+        let countdownBarDegree;
+        switch (timerMode.textContent) {
+            case 'focus':
+                countdownBarDegree = (360 / countdownBarRatios[0]) * (focusDuration - timeRemaining);
+                break;
+            case 'long break':
+                countdownBarDegree = (360 / countdownBarRatios[2]) * (longBreakDuration - timeRemaining);
+                break;
+            default:
+                countdownBarDegree = (360 / countdownBarRatios[1]) * (shortBreakDuration - timeRemaining);
+        }
 
-        timerNumber.innerHTML = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-        countdownBar.style.background = `conic-gradient(var(--body-background-color) ${countdownBarRatio * ((isFocus ? focusDuration : breakDuration) - timeRemaining)}deg, var(--countdown-cycle-bar-color) 0deg)`;
+        displayTimerNumber.innerHTML = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        countdownBar.style.background = `conic-gradient(var(--body-background-color) ${countdownBarDegree}deg, var(--countdown-cycle-bar-color) 0deg)`;
     };
 
     const timer = setInterval(() => {
         if (isPause) return;
 
-        timervisibility();
+        timerVisibility();
 
         if (timeRemaining === 0) {
             isFocus ^= 1;
             timeEndSound.play();
+
             if (isFocus) {
                 timerMode.textContent = 'focus';
                 timeRemaining = focusDuration;
             } else {
-                timerMode.textContent = 'break';
-                timeRemaining = breakDuration;
+                if (isEnableLongBreak) numbersCycle--;
+                if (numbersCycle <= 0) {
+                    timerMode.textContent = 'long break';
+                    timeRemaining = longBreakDuration;
+                    numbersCycle = parseInt(inputNumbersCycle.value);
+                } else {
+                    timerMode.textContent = isEnableLongBreak ? 'short break' : 'break';
+                    timeRemaining = shortBreakDuration;
+                }
             }
             pause();
-            timervisibility();
+            timerVisibility();
         } else timeRemaining--;
-    }, 1000);
+    }, 1);
 
     const pause = () => {
         isPause = true;
@@ -79,16 +125,16 @@ startButton.addEventListener('click', () => {
         isFocus = true;
 
         /* Reset visibility */
-        timervisibility();
+        timerVisibility();
         timerMode.textContent = 'focus';
         startButton.style.visibility = 'visible';
         pauseButton.style.visibility = 'hidden';
     });
 });
 
-const settingButton = document.getElementById('setting-btn');
 const settingWindow = document.getElementById('setting');
-settingButton.addEventListener('click', () => {
+
+document.getElementById('setting-btn').addEventListener('click', () => {
     settingWindow.style.display = 'block';
 });
 
@@ -97,16 +143,26 @@ closeSettingWindow.addEventListener('click', () => {
     settingWindow.style.display = 'none';
 });
 
-const saveSetting = document.getElementById('save-btn');
-saveSetting.addEventListener('click', () => {
-    pageLoad();
-    closeSettingPopup.click();
+document.getElementById('save-btn').addEventListener('click', () => {
+    pageLoading(false);
+    closeSettingWindow.click();
 });
 
-const resetSetting = document.getElementById('reset-btn');
-resetSetting.addEventListener('click', () => {
-    document.getElementById('focus-duration').value = DEFAULT_FOCUS_DURATION;
-    document.getElementById('break-duration').value = DEFAULT_BREAK_DURATION;
+document.getElementById('reset-btn').addEventListener('click', () => {
+    pageLoading();
 });
 
-document.addEventListener('DOMContentLoaded', pageLoad);
+checkboxLongBreak.addEventListener('change', function () {
+    let longBreakBox = document.querySelector('#long-break-box');
+    if (this.checked) {
+        longBreakBox.style.opacity = 1;
+        inputLongBreakDuration.disabled = false;
+        inputNumbersCycle.disabled = false;
+    } else {
+        longBreakBox.style.opacity = 0.5;
+        inputLongBreakDuration.disabled = true;
+        inputNumbersCycle.disabled = true;
+    }
+});
+
+document.addEventListener('DOMContentLoaded', pageLoading());
